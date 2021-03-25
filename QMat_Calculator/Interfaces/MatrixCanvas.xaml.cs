c63b33885aae3ade42475a07ef4ae22ad4480f99
ATBirch <U1761249@unimail.hud.ac.uk>
2021-03-25 10:37:24 +0000

    Began code cleanup for the matrix output.

--- QMat_Calculator/Interfaces/MatrixCanvas.xaml.cs
+++ QMat_Calculator/Interfaces/MatrixCanvas.xaml.cs
@@ -52,39 +52,17 @@ namespace QMat_Calculator.Interfaces
                 DisplayStep(step, i + 1);
             }
 
-
-            //string preceeder = FractionConverter.Convert(Convert.ToDouble(m.GetPreceder()));
-            //if (!String.IsNullOrWhiteSpace(preceeder))
-            //{
-            //    //if (preceeder.Contains('/')) { }
-            //    int midRow = Convert.ToInt32(Math.Round(Convert.ToDouble(m.getRows() / 2)));
-
-            //    Label l = new Label();
-            //    l.Content = preceeder;
-
-            //    Grid.SetRow(l, midRow);
-            //    Grid.SetColumn(l, 0);
-            //}
         }
 
         private void DisplayMatrix(Matrices.Matrix m)
         {
             dataGrid.RowDefinitions.Add(new RowDefinition());
 
-            Label l = new Label();
-            l.Content = ($"Solution: ");
+            int row = dataGrid.RowDefinitions.Count - 1;
 
-            Label s = new Label();
-            s.Content = m.ToString();
+            AddLabel($"Solution: ", dataGrid, row, 0);
+            AddLabel(m.ToString(), dataGrid, row, 1);
 
-            Grid.SetRow(l, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(l, 0);
-
-            Grid.SetRow(s, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(s, 1);
-
-            dataGrid.Children.Add(l);
-            dataGrid.Children.Add(s);
 
             Border b = new Border();
             b.BorderThickness = new Thickness(2);
@@ -95,98 +73,32 @@ namespace QMat_Calculator.Interfaces
 
             dataGrid.Children.Add(b);
 
-
-            //Complex[,] data = m.getData();
-
-            //for (int i = 0; i < m.getRows(); i++) { dataGrid.RowDefinitions.Add(new RowDefinition()); }
-            //for (int i = 0; i < m.getColumns(); i++) { dataGrid.ColumnDefinitions.Add(new ColumnDefinition()); }
-
-            //for (int c = 0; c < m.getColumns(); c++)
-            //{
-            //    for (int r = 0; r < m.getRows(); r++)
-            //    {
-            //        Label l = new Label();
-            //        Complex value = data[r, c];
-
-            //        StringBuilder sb = new StringBuilder();
-            //        sb.Append(String.Format("{0, -2}", value.Real));
-
-            //        if (value.Real == 0 && value.Imaginary != 0)
-            //        {
-            //            sb.Clear();
-            //        }
-            //        if (value.Imaginary != 0)
-            //        {
-            //            if (value.Imaginary == 1) { sb.Append(String.Format("{0, -2}", "+i")); }
-            //            else if (value.Imaginary == -1) { sb.Append(String.Format("{0, -2}", "-i")); }
-            //            else
-            //                sb.Append(String.Format("{0, -2}", value.Imaginary + "i"));
-            //        }
-            //        sb.Append(String.Format("{0, -2}", " "));
-
-            //        l.Content = sb.ToString();
-
-            //        Grid.SetRow(l, r);
-            //        Grid.SetColumn(l, c + 1);
-
-            //        dataGrid.Children.Add(l);
-            //    }
-            //}
         }
 
         private void DisplayStep(SolutionStep step, int stepNumber)
         {
             dataGrid.RowDefinitions.Add(new RowDefinition());
+            int row = dataGrid.RowDefinitions.Count - 1;
 
-            Label l = new Label();
-            l.Content = ($"Step {stepNumber}: ");
+            AddLabel($"Step {stepNumber}: ", dataGrid, row, 0);
+            AddLabel(step.getEquation(), dataGrid, row, 1);
+            AddLabel(step.getInput2().ToString(), dataGrid, row, 2);
+            AddLabel(step.FunctionString(), dataGrid, row, 3);
+            AddLabel(step.getInput1().ToString(), dataGrid, row, 4);
+            AddLabel("=", dataGrid, row, 5);
+            AddLabel(step.getAnswer().ToString(), dataGrid, row, 6);
 
-            Label e = new Label();
-            e.Content = step.getEquation();
+        }
 
-            Label i1 = new Label();
-            i1.Content = step.getInput1().ToString();
+        private void AddLabel(string content, Grid grid, int row, int column)
+        {
+            Label label = new Label();
+            label.Content = content;
 
-            Label f = new Label();
-            f.Content = step.FunctionString();
+            Grid.SetRow(label, row);
+            Grid.SetColumn(label, column);
 
-            Label i2 = new Label();
-            i2.Content = step.getInput2().ToString();
-
-            Label eq = new Label();
-            eq.Content = "=";
-
-            Label s = new Label();
-            s.Content = step.getAnswer().ToString();
-
-            Grid.SetRow(l, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(l, 0);
-
-            Grid.SetRow(e, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(e, 1);
-
-            Grid.SetRow(i1, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(i1, 4);
-
-            Grid.SetRow(f, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(f, 3);
-
-            Grid.SetRow(i2, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(i2, 2);
-
-            Grid.SetRow(eq, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(eq, 5);
-
-            Grid.SetRow(s, dataGrid.RowDefinitions.Count - 1);
-            Grid.SetColumn(s, 6);
-
-            dataGrid.Children.Add(l);
-            dataGrid.Children.Add(e);
-            dataGrid.Children.Add(i1);
-            dataGrid.Children.Add(f);
-            dataGrid.Children.Add(i2);
-            dataGrid.Children.Add(eq);
-            dataGrid.Children.Add(s);
+            grid.Children.Add(label);
         }
 
     }
